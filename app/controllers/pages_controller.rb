a16a6ffa3478979d71cc061f728b514060effd06
Faizan Haider <hfaizan53@gmail.com>
2022-06-29 23:56:18 +0500

    duke leads integeration

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,4 +1,16 @@
 class PagesController < ApplicationController
     def index
     end
+
+    def submit_lead
+
+        uri = URI("https://dukeleads.leadbyte.co.uk/api/submit.php?returnjson=yes&campid=FOREX&Email=#{params[:email]}&First_Name=#{params[:first_name]}&Last_Name=#{params[:last_name]}&Phone_1=#{params[:phone]}
+        ")
+        res = Net::HTTP.get_response(uri)
+        puts res.body  if res.is_a?(Net::HTTPSuccess)
+        redirect_to "/success"
+    end
+
+    def success
+    end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "pages#index"
+  post "/submit-lead", to: "pages#submit_lead"
+  get "/success", to: "pages#success"
 end
